b706a03241ef58e858dac36614b55cc53dddcbd3
test <test@tests-MBP.home>
2024-01-18 02:46:05 +0000

    adds milestone_4 file

---
+++ milestone_4.py
@@ -0,0 +1,38 @@
+import random
+class Hangman:
+    def __init__(self, word_list, num_lives=5):
+        self.word_list = word_list
+        self.num_lives = num_lives
+
+        self.word = random.choice(self.word_list)
+        self.word_guessed = ['_'] * len(self.word)
+        self.num_letters = len(set(self.word))
+        self.list_of_guesses = []
+
+    def check_guess(self, guess): 
+        if guess in self.word:
+            print(f"Good guess! {guess} is in the word.")
+            for index in range(len(self.word)):
+                if self.word[index] == guess: 
+                    self.word_guessed[index] = self.word[index]
+            self.num_letters -= 1
+        else: 
+            self.num_lives -= 1
+            print(f"Sorry, {guess} is not in the word. Try again.")
+            print(f"You have {self.num_lives} lives left.")
+
+    def ask_for_input(self):
+        while True:
+            print(self.word_guessed)
+            guess = input("Guess a letter: ").lower()
+            if len(guess) != 1 or not guess.isalpha():
+                print("Invalid letter. Please, enter a single alphabetical character.")
+            elif guess in self.list_of_guesses:
+                print("You already tried that letter!")
+            else: 
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess)
+
+
+h = Hangman(['strawberry'])
+h.ask_for_input()
